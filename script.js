09e925ecfc0b049f55fe7cdf978befa6751cc838
inputduck <inputduck@gmail.com>
2022-02-08 15:38:31 +0000

    js complete. design elements started.

--- script.js
+++ script.js
@@ -1,41 +1,73 @@
-let messageEl = document.getElementById("message-el");
+let num
 let choice = '';
-
-
-
+let compChoice = '';
 
 
 function rock() {
-    choice = 'rock'
-    let num = Math.floor(Math.random()) * 3;
-    document.getElementById("message-el").innerHTML = num;
-    console.log('Rock was clicked');
-    return choice
-   
+    choice = 'rock';
+    compMove('rock')
 }
 
 function paper() {
-    choice = 'paper'
-    document.getElementById("message-el").innerHTML = "PAPER";
-    console.log('Paper was clicked');
-    return choice
+    choice = 'paper';
+    compMove('paper')
 }
 
 function scissors() {
-    choice = 'scissors'
-    document.getElementById("message-el").innerHTML = "SCISSORS";
-    console.log('Scissors was clicked');
-    return choice
+    choice = 'scissors';
+    compMove('scissors')
 }
 
 function compMove() {
-
+    let num = Math.floor(Math.random() * 3);
+    if (num == 0) {
+        compChoice = 'rock';
+        rps(choice, compChoice);
+    } else if (num == 1) {
+        compChoice = 'paper';
+        rps(choice, compChoice);
+    } else {
+        compChoice = 'scissors';
+        rps(choice, compChoice);
+    }
+        //document.getElementById("message-el").innerText = `Computer picked ${compChoice}`;       
+}
+    
+function rps(choice, compChoice) {
+    if (choice === compChoice) {
+        document.getElementById("message-el").innerText = `Computer picked ${compChoice}. Draw!`;
+    } else if (choice == 'rock') {
+        switch (compChoice) {
+            case 'paper':
+                document.getElementById("message-el").innerText = `Computer picked ${compChoice}. Computer wins!`;
+                break;
+            case 'scissors':
+                document.getElementById("message-el").innerText = `Computer picked ${compChoice} You win!`;
+                break;
+        }
+    } else if (choice == 'paper') {
+        switch (compChoice) {
+            case 'rock':
+                document.getElementById("message-el").innerText = `Computer picked ${compChoice} You win!`;
+                break;
+            case 'scissors':
+                document.getElementById("message-el").innerText = `Computer picked ${compChoice}. Computer wins!`;
+                break;
+        }
+    } else if (choice == 'scissors') {
+        switch (compChoice) {
+            case 'rock':
+                document.getElementById("message-el").innerText = `Computer picked ${compChoice} Computer wins!`;
+                break;
+            case 'paper':
+                document.getElementById("message-el").innerText = `Computer picked ${compChoice} You win!`;
+                break;
+        }
+    }
 }
 
 
-startGame() {
 
-}
 
 
 
