66c05ca1705933b6a50170e7327ca95d21ae04db
inputduck <inputduck@gmail.com>
2022-02-08 11:17:26 +0000

    javascript adjustments

--- script.js
+++ script.js
@@ -1,17 +1,46 @@
-//This is the JS only version of the script. Needs to be modified to make it work with buttons and HTML.
+let messageEl = document.getElementById("message-el");
+let choice = '';
+
+
+
+
 
 function rock() {
-console.log('Rock was clicked');
+    choice = 'rock'
+    let num = Math.floor(Math.random()) * 3;
+    document.getElementById("message-el").innerHTML = num;
+    console.log('Rock was clicked');
+    return choice
+   
 }
 
 function paper() {
+    choice = 'paper'
+    document.getElementById("message-el").innerHTML = "PAPER";
     console.log('Paper was clicked');
+    return choice
 }
 
 function scissors() {
+    choice = 'scissors'
+    document.getElementById("message-el").innerHTML = "SCISSORS";
     console.log('Scissors was clicked');
+    return choice
 }
 
+function compMove() {
+
+}
+
+
+startGame() {
+
+}
+
+
+
+
+//This is the JS only version of the script. Needs to be modified to make it work with buttons and HTML.
 
 /*function rps(p1, p2) {
     if (p1 === p2) {
