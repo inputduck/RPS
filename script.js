760083a4a7d46f691ee27cc62185f8e71fd2a0bd
inputduck <inputduck@gmail.com>
2022-03-01 16:12:03 +0000

    changed everything

--- script.js
+++ script.js
@@ -1,79 +1,135 @@
 let num
 let choice = '';
 let compChoice = '';
+
 let slogans = ['Do we go on 3...or after 3?', 'You go first', 'Which one is scissors again?', 'What? No Spock button?', 'I promise to pick paper this time', 'Hi Tori', 'No Cheating', 'Best of three?', 'What is this human emotion you call "Vengeance"?','You will soon be travelling negatively along the y-axis', 'RNG stabilisers are active', 'Have you ever met my brother HAL?' ]
 
 
 
-function rock() {
-    choice = 'rock';
-    compMove('rock')
-}
+const moveButtons = document.getElementsByClassName('move-btn')
+const movesDisplay = document.getElementById('movesDisplay')
+const buttons = document.getElementById('buttons');
+const p1MoveShow = document.getElementById('p1MoveShow');
+const p1MoveText = document.getElementById('p1MoveText');
+const compMoveShow = document.getElementById('compMoveShow');
+const compMoveText = document.getElementById('compMoveText');
+const playAgain = document.getElementById('playAgainButton')
 
-function paper() {
-    choice = 'paper';
-    compMove('paper')
-}
 
-function scissors() {
-    choice = 'scissors';
-    compMove('scissors')
+
+function gameInit() {
+    sloganDisplay();
+    document.getElementById('sloganDisplay').style.display = "inline";
+    document.getElementById('instruction').style.display = "inline";
+    document.getElementById('rock_btn').style.display = "inline";
+    document.getElementById('paper_btn').style.display = "inline";
+    document.getElementById('scissors_btn').style.display = "inline";
+    document.getElementById('resultsMessage').style.display = "none";
+    playAgainButton.style.display = "none";
+    //movesDisplay.style.visibility = "hidden";
+    
+    
+    
+    
 }
 
 function sloganDisplay() {
     let sloganChoice = Math.floor(Math.random() * slogans.length);
-    document.getElementById("message-el").innerText = slogans[sloganChoice];
-    console.log(sloganChoice);
+    document.getElementById("sloganDisplay").innerText = slogans[sloganChoice];
+    
+}
+
+function moveSelect(choice) {
+    document.getElementById('sloganDisplay').style.display = "none";
+    document.getElementById('instruction').style.display = "none";
+    
+    console.log(choice)
+   // movesDisplay.style.visibility = "visible"
+    //p1MoveText.textContent = choice
+    compMove(choice)
 }
 
 
-function compMove() {
+
+
+
+
+
+function compMove(choice) {
+    //movesDisplay.style.visibility = "visible"
     let num = Math.floor(Math.random() * 3);
     if (num == 0) {
         compChoice = 'rock';
+        //compMoveText.innerText = `${compChoice}`;
         rps(choice, compChoice);
     } else if (num == 1) {
         compChoice = 'paper';
+        //compMoveText.innerText = `${compChoice}`;
         rps(choice, compChoice);
     } else {
         compChoice = 'scissors';
+        //compMoveText.innerText = `${compChoice}`;
         rps(choice, compChoice);
     }
         //document.getElementById("message-el").innerText = `Computer picked ${compChoice}`;       
 }
     
 function rps(choice, compChoice) {
+//      playAgainButton.style.display = "inline";
+//      movesDisplay.style.display = "inline";
+//      movesMessage.style.display = "inline";
+document.getElementById('resultsMessage').style.display = "inline";
     if (choice === compChoice) {
-        document.getElementById("message-el").innerText = `Computer picked ${compChoice}.` + `\nIt's a Draw!`;
+        
+        document.getElementById("resultsMessage").innerText = `Computer picked ${compChoice}.` + `\n\nIt's a Draw!`;
+        gameFinish()
     } else if (choice == 'rock') {
         switch (compChoice) {
             case 'paper':
-                document.getElementById("message-el").innerText = `Computer picked ${compChoice}.` + `\nComputer wins!`;
+                document.getElementById("resultsMessage").innerText = `Computer picked ${compChoice}.` + `\n\nComputer wins!`;
+                gameFinish()
                 break;
             case 'scissors':
-                document.getElementById("message-el").innerText = `Computer picked ${compChoice}.` + `\nYou win!`;
+                document.getElementById("resultsMessage").innerText = `Computer picked ${compChoice}.` + `\n\nYou win!`;
+                gameFinish()
                 break;
         }
     } else if (choice == 'paper') {
         switch (compChoice) {
             case 'rock':
-                document.getElementById("message-el").innerText = `Computer picked ${compChoice}.` + `\nYou win!`;
+                document.getElementById("resultsMessage").innerText = `Computer picked ${compChoice}.` + `\n\nYou win!`;
+                gameFinish()
                 break;
             case 'scissors':
-                document.getElementById("message-el").innerText = `Computer picked ${compChoice}.` + `\nComputer wins!`;
+                document.getElementById("resultsMessage").innerText = `Computer picked ${compChoice}.` + `\n\nComputer wins!`;
+                gameFinish()
                 break;
         }
     } else if (choice == 'scissors') {
         switch (compChoice) {
             case 'rock':
-                document.getElementById("message-el").innerText = `Computer picked ${compChoice}.` + `\nComputer wins!`;
+                document.getElementById("resultsMessage").innerText = `Computer picked ${compChoice}.` + `\n\nComputer wins!`;
+                gameFinish()
                 break;
             case 'paper':
-                document.getElementById("message-el").innerText = `Computer picked ${compChoice}.` + `\nYou win!`;
+                document.getElementById("resultsMessage").innerText = `Computer picked ${compChoice}.` + `\n\nsYou win!`;
+                gameFinish()
                 break;
         }
     }
     
+}
+
+function gameFinish() {
+    document.getElementById('rock_btn').style.display = "none"
+    document.getElementById('paper_btn').style.display = "none"
+    document.getElementById('scissors_btn').style.display = "none"
+    //rockButton.style.display = "none";
+
+    playAgainButton.style.display = "inline"
+    console.log('game finished')
+
+
 }
 
 
@@ -116,3 +172,4 @@ function rps(choice, compChoice) {
         }
     }
 } */
+
